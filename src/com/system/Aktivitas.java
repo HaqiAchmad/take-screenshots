7890c854d52a265eaf039bee62f24398a0c7690c
Achmad Baihaqi <hakiahmad756@gmail.com>
2020-06-11 15:25:08 +0700

    mengupdate class Aktivitas.java

--- src/com/system/Aktivitas.java
+++ src/com/system/Aktivitas.java
@@ -14,18 +14,36 @@ import javax.swing.JOptionPane;
  * 
  * 
  * Type class : Superclass
- * Total method : 14
- * Total line : 369 
+ * Total method : 16
+ * Total line : 274
  * 
  * @author Achmad Baihaqi
- * @version 1.0
+ * @version 1.2
  * @since 15-05-2020
  */
 public class Aktivitas {
 
     /**
+     * File yang berada didalam database
+     */
+     private static File fileAbout = new File("database\\about.haqi"),
+                         fileAktivitas = new File("database\\aktivitas.haqi"),
+                         fileDate = new File("database\\date.haqi"),
+                         fileIsActive = new File ("database\\isActive.haqi"),
+                         fileIsAutoSave = new File("database\\isAutoSave.haqi"),
+                         fileIsOpen = new File("database\\isOpen.haqi"),
+                         fileLanguage = new File("database\\language.haqi"),
+                         fileSaveTo = new File("database\\SaveTo.haqi"),
+                         fileScreenshots = new File("database\\screenshots.haqi"),
+                         fileTheme = new File("database\\theme.haqi"),
+                         fileTotalSS = new File("database\\TotalSS.haqi");
+
+
+
+
+    /**
      * 
-     * Bertugas untuk menuliskan semua aktivitas yang dilakukan user ke system tepatnya ke file aktivitas.haqi pada package com.database
+     * Bertugas untuk menuliskan semua aktivitas yang dilakukan user ke system tepatnya ke file aktivitas.haqi
      * Method ini akan menulikan aktivitas user ke file tersebut pada baris baru 
      * 
      * 
@@ -34,101 +52,56 @@ public class Aktivitas {
      * @param aktivitas digunakan sebagai input, input disini adalah aktivitas yang dilakukan user
      */
     public static void addAktivitas(String aktivitas){
-        
-        try{
-            FileWriter file = new FileWriter("src\\com\\database\\aktivitas.haqi", true);
-            BufferedWriter tulis = new BufferedWriter(file);
-            
-            tulis.write(aktivitas); tulis.newLine(); tulis.flush();
-            
-        }catch(IOException ex){
-            ex.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Terjadi kesalahan di (Aktivitas.java)\n" + ex);
-        }
+        Files.writeFile(fileAktivitas.toString(), aktivitas, true);
     }
     
     /**
      * Berfungsi untuk menampilakan semua aktivitas yang dilakukan user ke konsol/screen
-     * Dengan membaca file aktivitas.haqi pada package com.database
+     * Dengan membaca file aktivitas.haqi
      * 
      * @see FileReader
      * @see BufferedWriter
-     * @exception IOExcepion jika file tidak ada atau kosong
      */
     private static void showAktivitas(){
-        
-        try{
-            FileReader file = new FileReader("src\\com\\database\\aktivitas.haqi");
-            BufferedReader baca = new BufferedReader(file);
-            String buffer = baca.readLine();
-            
-                // membaca keseluruhan file aktivitas.haqi
-                while(buffer != null){
-                    System.out.println(buffer);
-                    buffer = baca.readLine();
-                }
-            
-            
-        }catch(IOException ex){
-            ex.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Terjadi kesalahan di (Aktivitas.java)\n" + ex);
-        }
-        
+        Files.showFile(fileAktivitas.toString());
     }
     
     /**
      * Method berfungsi untuk mengetaui total screenshot yang diambil user
-     * Dengan mengambil data dari file TotalSS.haqi dipackage com.database
+     * Dengan mengambil data dari file TotalSS.haqi melaui class Files
      * Method ini juga berfungsi untuk penamaan file hasil sceenshot
      * 
-     * Note : jik file kosong maka akan mereturn 1
+     * Note : jika file kosong maka akan mereturn 1
      * 
-     * @see FileReader
-     * @see BufferedReader
+     * @see Files
+     *
      * @return total screenshot yang diambil
      */
     public static int getTotalSS(){
-                
-        try{
-            FileReader file = new FileReader("src\\com\\database\\TotalSS.haqi");
-            BufferedReader baca = new BufferedReader(file);
-            String buffer = baca.readLine();
-            
-            return Integer.parseInt(buffer);
-            
-        }catch(IOException ex){
-            ex.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Terjadi kesalahan di (Aktivitas.java)\n" + ex);
+        String totalSS = Files.getDataFile(fileTotalSS.toString());
+        if(totalSS == null){
+            return 0;
+        }else{
+            return Integer.parseInt(totalSS);
         }
-        return 1;
     }
 
     /**
      * Berfungsi untuk mengupdate total screenshot yang diambil. "total sceenshot akan selalu diupdate saat user menekan tombol take scrensot pada com.window.Root() "
-     * Method ini akan mengkonversi totalSS ke string dan menuliskanya ke file TotalSS.haqi di package com.database
-     * Method ini akan menimpa data total ss sebelumnya (append = false)
+     * Method ini akan mengkonversi totalSS ke string dan menuliskanya ke file TotalSS.haqi
+     * Method ini akan menimpa data total ss sebelumnya
      * 
      * @see FileWriter 
      * @see BufferedWriter
      * @param totalSS input total jumlah screenthot
      */
     public static void setTotalSS(int totalSS){
-        
-        try{
-            
-            FileWriter file = new FileWriter("src\\com\\database\\TotalSS.haqi");
-            BufferedWriter tulis = new BufferedWriter(file);
-            
-            tulis.write(Integer.toString(totalSS)); tulis.newLine(); tulis.flush();
-            
-        }catch(IOException ex){
-            ex.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Terjadi kesalahan di (Aktivitas.java)\n" + ex);
-        }
+
+        Files.writeFile(fileTotalSS.toString(), Integer.toString(totalSS),false);
     }
     
     /**
-     * Menampilkan semua data yang ada didalam file sceeenshots.haqi di package com.database
+     * Menampilkan semua data yang ada didalam file sceeenshots.haqi ke console/layar
      * Data tersebut berisi direktori dari screenshot yang diambil user
      * 
      * @see FileReader 
@@ -136,27 +109,11 @@ public class Aktivitas {
      * 
      */
     public static void showScreenshots(){
-        
-        try{
-            
-            FileReader file = new FileReader("src\\com\\database\\screenshots.haqi");
-            BufferedReader baca = new BufferedReader(file);
-            String buffer = baca.readLine();
-            
-                while(buffer != null){
-                    System.out.println(buffer);
-                    buffer = baca.readLine();
-                }
-            
-        }catch(IOException ex){
-            ex.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Terjadi kesalahan di (Aktivitas.java)\n" + ex);
-        }
-        
+        Files.showFile(fileScreenshots.toString());
     }
     
     /**
-     * Menuliskan direktori screenshot ke file screenshots.haqi dipackage com.database
+     * Menuliskan direktori screenshot ke file screenshots.haqi
      * Direktori akan dituliskan pada baris baru (append)
      * 
      * @see FileWriter
@@ -164,23 +121,12 @@ public class Aktivitas {
      * @param screenshot input direktori screenshot yang diambil user
      */
     public static void addScreenshot(String screenshot){
-        
-        try{
-            
-            FileWriter file = new FileWriter("src\\com\\database\\screenshots.haqi", true);
-            BufferedWriter tulis = new BufferedWriter(file);
-            
-            tulis.write(screenshot); tulis.newLine(); tulis.flush();
-            
-        }catch(IOException ex){
-            ex.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Terjadi kesalahan di (Aktivitas.java)\n" + ex);
-        }
+        Files.writeFile(fileScreenshots.toString(), screenshot, true);
     }
     
     /**
      * Untuk mengatur apakan aplikasi sudah terbuka atau belum
-     * Dengan menuliskan open(input) ke file isopen.haqi dipackage com.database dan menimpa data sebelumnya(append = false)
+     * Dengan menuliskan open(input) ke file isopen.haqi dan menimpa data sebelumnya(append = false)
      * Key(open) hanya memiliki dua nilai yaitu:
      *  open: berarti aplikasi saat ini sedang dibuka
      *  close: berarti aplikasi saat ini sedang tidak dibuka
@@ -192,50 +138,13 @@ public class Aktivitas {
      * @param key hanya dapat bernilai open atau close
      */
     public static void setOpen(String key){
-        
-        
-        try{
-            
-            FileWriter file = new FileWriter("src\\com\\database\\isopen.haqi");
-            BufferedWriter tulis = new BufferedWriter(file);
-            
-                if(key.equalsIgnoreCase("open") || key.equalsIgnoreCase("close")){
-                    tulis.write(key); tulis.newLine(); tulis.flush();
-                }else{
-                    tulis.write("close");
-                }
-            
-        }catch(IOException ex){
-            ex.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Terjadi kesalahan di (Aktivitas.java)\n" + ex);
+        if(key.equalsIgnoreCase("open")){
+            Files.writeFile(fileIsOpen.toString(), key, false);
+        }else{
+            Files.writeFile(fileIsOpen.toString(), key, false);
         }
-        
     }
-    
-    /**
-     * Untuk mendapatkan data dari file isopen.haqi dipackage com.database
-     * Jika file kosong maka akan return close
-     * 
-     * @see FileReader
-     * @see BufferedReader
-     * @return data dari file isopen.haqi
-     */
-    private static String getOpen(){
-                
-        try{
-            FileReader file = new FileReader("src\\com\\database\\isopen.haqi");
-            BufferedReader baca = new BufferedReader(file);
-            String buffer = baca.readLine();
-            
-            return buffer;
-            
-        }catch(IOException ex){
-            ex.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Terjadi kesalahan di (Aktivitas.java)\n" + ex);
-        }
-        return "close";
-    }
-    
+
     /**
      * Method ini digunakan untuk mengetahui apakan aplikasi sudah terbuka atau belum
      * 
@@ -244,8 +153,10 @@ public class Aktivitas {
      * @return jika sudah mereturn true, jika belum mereturn false 
      */
     public static boolean isOpen_aplikasi(){
-        
-        if(Aktivitas.getOpen().contains("open")){
+        String data = Files.getDataFile(fileIsOpen.toString());
+        if(data == null){
+            return false;
+        }else if(data.equals("open")){
             return true;
         }else{
             return false;
@@ -253,7 +164,7 @@ public class Aktivitas {
     }
     
     /**
-     * Method akan menuliskan autosave(input) ke file isautosave.haqi pada package com.database
+     * Method akan menuliskan autosave(input) ke file isautosave.haqi
      * Input yang dituliskan hanya memiliki dua nilai yaitu:
      *  actived : user mengaktifkan setelan untuk autosave
      *  deactived : user menonaktifkan setelan untuk autosave
@@ -265,26 +176,7 @@ public class Aktivitas {
      * @param autosave input hanya dpt bernilai actived atau deactived
      */
     public static void setAutoSave(String autosave){
-
-        try{
-            
-            FileWriter file = new FileWriter("src\\com\\database\\isautosave.haqi");
-            BufferedWriter tulis = new BufferedWriter(file);
-            
-                if(autosave.equalsIgnoreCase("actived")){
-                    tulis.write(autosave); tulis.newLine(); tulis.flush();
-                }else if(autosave.equalsIgnoreCase("deactived")){
-                    tulis.write(autosave); tulis.newLine(); tulis.flush();
-                }else{
-                    tulis.write("deactived"); tulis.newLine(); tulis.flush();
-                }
-               
-            
-        }catch(IOException ex){
-            ex.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Terjadi kesalahan di (Aktivitas.java)\n" + ex);
-        }
-        
+        Files.writeFile(fileIsAutoSave.toString(), autosave, false);
     }
     
     /**
@@ -295,27 +187,53 @@ public class Aktivitas {
      * @return jika ya maka mereturn true, jika tidak maka mereturn false
      */
     public static boolean isAutoSave(){
-    
-        try{
-            FileReader file = new FileReader("src\\com\\database\\isautosave.haqi");
-            BufferedReader baca = new BufferedReader(file);
-            String buffer = baca.readLine();
-            
-            if(buffer.equalsIgnoreCase("actived")){
-                return true;
-            }else{ // deactived
-                return false;
-            }
-            
-        }catch(IOException ex){
-            ex.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Terjadi kesalahan di (Aktivitas.java)\n" + ex);
+        String data = Files.getDataFile(fileIsAutoSave.toString());
+
+        if(data == null){
+            return false;
+        }else if(data.equalsIgnoreCase("actived")){
+            return true;
+        }else{
+            return false;
         }
-        return true;
     }
-           
+
     /**
-     * Untuk menuliskan direktori file yang akan diproes ke file isactive.haqi pada package com.database
+     * Method ini digunakan untuk mendapatkan direktori yang diatur user untuk menyimpan hasil screenshot
+     * Dengan membaca file SaveTo.haqi
+     * Lalu method akan mereturn data yang ada difile SaveTo.haqi
+     * Jika file SaveTo.haqi kosong maka method akan mereturn direktori userhome\\downloads sebagai default Example : "C:\\Users\\YOU\\Downloads\\
+     *
+     * @see FileReader
+     * @see BufferedReader
+     *
+     * @return direktori untuk menyimpan file hasil screenshot
+     */
+    public static String getPenyimpanan() {
+        String storage = Files.getDataFile(fileSaveTo.toString());
+        if (storage == null) {
+            return "C:\\Users\\" + Apps.getUsername() + "\\Downloads\\";
+        } else {
+            return storage;
+        }
+    }
+
+    /**
+     * Method ini digunakan untuk menyimpan direktori peyimpanan screenshot yang diatur user
+     * Direktori akan disimpan kefile SaveTo.haqi
+     * User dapat menguabah setelan direktori di window com.window.Root.java
+     *
+     * @see FileWriter
+     * @see BufferedWriter
+     *
+     * @param simpanKe input direktori yang diatur user
+     */
+    public static void setPeyimpanan(String simpanKe) {
+        Files.writeFile(fileSaveTo.toString(), simpanKe, false);
+    }
+
+    /**
+     * Untuk menuliskan direktori file yang akan diproes ke file isactive.haqi
      * Yang dimaksud "file yang akan diproses" adalah Opsi yang ditampilkan setelah screenshot diambil(com.window.SimpanGambar.java)
      * 
      * @see FileWriter
@@ -323,47 +241,22 @@ public class Aktivitas {
      * @param aktif direktori file yang akan diproses 
      */
     public static void setAktif(String aktif){
-        
-        
-        try{
-            
-            FileWriter file = new FileWriter("src\\com\\database\\isactive.haqi");
-            BufferedWriter tulis = new BufferedWriter(file);
-            
-               tulis.write(aktif); tulis.newLine(); tulis.flush();
-            
-        }catch(IOException ex){
-            ex.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Terjadi kesalahan di (Aktivitas.java)\n" + ex);
-        }
-        
+        Files.writeFile(fileIsActive.toString(), aktif, false);
     }
     
     /**
      * Mendapatkan file yang sedang aktif(file yang sedang diproses pada SimpanGambar.java)
-     * Data diambil dari file isactive.haqi pada package com.database
+     * Data diambil dari file isactive.haqi
      * Method ini juga digunakan untuk mendapatkan data-data dari file  yang sedang di proses seperti (nama file, format file, ukuran file, resolusi file, dll)
-     * 
-     * Note : jika file kosong maka akan return null
+     *
      * 
      * @see FileReader 
      * @see BufferedReader
-     * @return  
+     * @return  file yang sedang aktif
      */
     public static String getAktif(){
-                
-        try{
-            FileReader file = new FileReader("src\\com\\database\\isactive.haqi");
-            BufferedReader baca = new BufferedReader(file);
-            String buffer = baca.readLine();
-            
-            return buffer;
-            
-        }catch(IOException ex){
-            ex.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Terjadi kesalahan di (Aktivitas.java)\n" + ex);
-        }
-        return "null";
+        return Files.getDataFile(fileIsActive.toString());
     }
-    
+
+
 }
