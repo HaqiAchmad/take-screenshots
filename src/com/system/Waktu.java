1c6d4a9224f05849c642b07c5afa459d6dd86734
Achmad Baihaqi <hakiahmad756@gmail.com>
2021-02-26 00:55:26 +0700

    mengupdate class Waktu.java

--- src/com/system/Waktu.java
+++ src/com/system/Waktu.java
@@ -1,16 +1,14 @@
 package com.system;
 
-import java.time.LocalDateTime;
-
+import java.util.Calendar;
 
 /**
  * Class ini digunakan untuk mengetahui waktu/tanggal saat ini
- * Berguna untuk penamaan file pada hasil screenshot dan penulisan informasi ke database
- * Class ini mendapatkan informasi waktu/tanggal saat ini melalui class LocalDateTime 
- * 
+ * Berguna untuk penamaan file pada hasil screenshot dan penulisan informasi ke log file
+ * Class ini mendapatkan informasi waktu/tanggal saat ini melalui class Calendar
  * 
  * @author Achmad Baihaqi
- * @version 1.2
+ * @version 1.3
  * @since 12 05 2020
  */
 public class Waktu {
@@ -18,7 +16,7 @@ public class Waktu {
     /**
      * Digunakan untuk mendapatkan waktu saat ini
      */
-    private static final LocalDateTime local = LocalDateTime.now();
+    private static Calendar time = Calendar.getInstance();
 
     /**
      * Attribute yang akan digunakan untuk menyimpan data dari waktu
@@ -30,14 +28,34 @@ public class Waktu {
      */
     private static String namaHari, namaBulan;
     
+    public Waktu(){
+        updateTime();
+    }
+    
+    /**
+     * Mendapatkan waktu saat ini dengan menggunakan object time
+     */
+    public static void updateTime(){
+        time = Calendar.getInstance();
+        detik = time.get(Calendar.SECOND);
+        menit = time.get(Calendar.MINUTE);
+        jam = time.get(Calendar.HOUR_OF_DAY);
+        tanggal = time.get(Calendar.DAY_OF_MONTH);
+        bulan = time.get(Calendar.MONTH);
+        tahun = time.get(Calendar.YEAR);
+        namaHari = getNamaHari();
+        namaBulan = getNamaBulan();
+    }
+    
     /**
      * Method ini digunakan untuk mendapatkan informasi tentang tanggal saat ini.
-     * Method ini digunakan untuk aktivitas
+     * Method ini digunakan untuk penulisan log aplikasi
      * 
      * 
      * @return Tanngal dan jam saat ini Example :  Selasa, 12 January 2020 13:34
      */
-    public static String getTanggal_Activity(){
+    public static String getTanggal_Aktivitas(){
+        updateTime();
         return String.format("%s, %d %s %d %02d:%02d", 
                     Waktu.namaHari, Waktu.tanggal, Waktu.namaBulan, Waktu.tahun, Waktu.jam, Waktu.menit 
                );
@@ -51,149 +69,53 @@ public class Waktu {
      * @return Tanngal dan jam saat ini Example :  2020-1-12 13.36.10
      */
     public static String getTanggal_Screenshot(){
+        updateTime();
         return String.format("%d-%d-%d %02d.%02d.%02d", 
-                Waktu.tahun, Waktu.bulan, Waktu.tanggal, Waktu.jam, Waktu.menit, Waktu.detik
+                Waktu.tahun, Waktu.bulan+1, Waktu.tanggal, Waktu.jam, Waktu.menit, Waktu.detik
                 );
     }
     
     /**
      * Method ini digunakan untuk mendapatkan nama hari saat ini
-     * Method ini mendapatkan nama hari berdasarkan kode hari yang didapat dari attribute codehari
-     * Kode hari berkisar antara 1 - 7
-     *  1 berati senin, 2 berarti selasa, 3 berati rabu dan seterusnya sampai 7
-     * 
      * 
      * @return nama hari saat ini 
      */
     private static String getNamaHari(){
         
-        switch(Waktu.codehari){
-            case 1: return "Senin"; 
-            case 2: return "Selasa";
-            case 3: return "Rabu";
-            case 4: return "Kamis";
-            case 5: return "Jumat";
-            case 6: return "Sabtu";
-            case 7: return "Minggu";
-            default: return "null";      
+        switch(time.get(Calendar.DAY_OF_WEEK)){
+            case Calendar.SUNDAY: return "Minggu";
+            case Calendar.MONDAY: return "Senin";
+            case Calendar.TUESDAY: return "Selasa";
+            case Calendar.WEDNESDAY: return "Rabu";
+            case Calendar.THURSDAY: return "Kamis";
+            case Calendar.FRIDAY: return "Jumat";
+            case Calendar.SATURDAY: return "Sabtu";
+            default: return "null";
         }
-        
     }
     
     /**
      * Method ini digunakan untuk mendapatkan nama bulan saat ini.
-     * Method ini mendapatkan nama bulan berdasarkan nilai bulan dari attribute bulan pada class ini.
-     * Nilai bulan berkisar antara 1 - 12
-     *  1 berati januari, 2 berarti februari, 3 berati maret dan seterusnya sampai 12.
-     * 
      * 
      * @return nama bulan saat ini 
      */
     private static String getNamaBulan(){
         
-        switch(Waktu.bulan){
-            case 1: return "Januari"; 
-            case 2: return "Februari";
-            case 3: return "Maret";
-            case 4: return "April";
-            case 5: return "Mei";
-            case 6: return "Juni";
-            case 7: return "Juli";
-            case 8: return "Agustus"; 
-            case 9: return "September";
-            case 10: return "Oktober";
-            case 11: return "November";
-            case 12: return "Desember";
+        switch(time.get(Calendar.MONTH)){
+            case Calendar.JANUARY: return "Januari"; 
+            case Calendar.FEBRUARY: return "Februari";
+            case Calendar.MARCH: return "Maret";
+            case Calendar.APRIL: return "April";
+            case Calendar.MAY: return "Mei";
+            case Calendar.JUNE: return "Juni";
+            case Calendar.JULY: return "Juli";
+            case Calendar.AUGUST: return "Agustus"; 
+            case Calendar.SEPTEMBER: return "September";
+            case Calendar.OCTOBER: return "Oktober";
+            case Calendar.NOVEMBER: return "November";
+            case Calendar.DECEMBER: return "Desember";
             default: return "null";  
         }
         
     }
-    
-    /**
-     * Digunakan untuk mendapatkan jumlah hari dalam bulan tertentu
-     * class ini menggunakan attribute bulan untuk mendapatkan nilai dari bulan dan mereturn jumlah harinya
-     * 
-     * @return jumlah hari dalam sebulan
-     */
-    protected static int jumlahHari(){
-        
-        switch(Waktu.bulan){
-            case 1: return 31; 
-            case 2: return (Waktu.tahun % 4 == 0) ? (29) : (28);
-            case 3: return 31;
-            case 4: return 31;
-            case 5: return 31;
-            case 6: return 30;
-            case 7: return 31;
-            case 8: return 31;
-            case 9: return 30;
-            case 10: return 31;
-            case 11: return 30;
-            case 12: return 31;
-            default : return 1;
-                
-        }
-        
-    }
-    
-    /**
-     * Method ini digunakan untuk mendapatkan detik, menit, jam, tanggal, codehari, namahari, bulan, namabulan dan tahun saat ini.
-     * 
-     */
-    public static void updateTime(){
-        
-        // untuk mendapatkan waktu saat ini 
-        Waktu.detik = local.getSecond();
-        Waktu.menit = local.getMinute();
-        Waktu.jam = local.getHour();
-        Waktu.tanggal = local.getDayOfMonth();
-        Waktu.codehari = local.getDayOfWeek().getValue();
-        Waktu.namaHari = Waktu.getNamaHari();
-        Waktu.bulan = local.getMonthValue();
-        Waktu.namaBulan = Waktu.getNamaBulan();
-        Waktu.tahun = local.getYear();
-
-        // mengupdate waktu
-            new Thread(new Runnable(){
-                
-                @Override
-                public void run(){
-                    try{
-                        // waktu akan terus diupdate sampai aplikasi diclose
-                        while(true){
-
-                            Waktu.detik++;
-                            if(Waktu.detik > 59){
-                                Waktu.detik = 0;
-                                Waktu.menit++;
-                            }else if(Waktu.menit > 59){
-                                Waktu.menit = 0;
-                                Waktu.jam++;
-                            }else if(Waktu.jam > 23){
-                                Waktu.jam = 0;
-                                Waktu.tanggal++;
-                                Waktu.codehari++;
-                                    if(Waktu.codehari > 7){
-                                        Waktu.codehari = 0;
-                                    }
-                            }else if(Waktu.tanggal > Waktu.jumlahHari()){
-                                Waktu.tanggal = 1;
-                                Waktu.bulan++;
-                            }else if(Waktu.bulan > 12){
-                                Waktu.bulan = 1;
-                                Waktu.tahun++;
-                            }
-
-                           Thread.sleep(1000);
-
-                        }
-                        
-                    }catch(InterruptedException iex){
-                        javax.swing.JOptionPane.showMessageDialog(null, iex);
-                    }
-                }
-            
-            }).start();
-    }
-
 }
