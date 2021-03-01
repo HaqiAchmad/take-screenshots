6496349eb0b8c8be9f74c15a3249cf39c10366d1
Achmad Baihaqi <hakiahmad756@gmail.com>
2021-03-01 23:38:24 +0700

    mengupdate class Main.java

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -4,6 +4,7 @@ import com.system.Aktivitas;
 import com.system.Apps;
 import com.database.Database;
 import com.system.Waktu;
+import com.window.MainWindow;
 
 import java.io.IOException;
 
@@ -21,23 +22,22 @@ public class Main {
      * Method main ini digunakan untuk membuka aplikasi
      * Sebelum membuka aplikasi method akan mengecek apakah database exist atau tidak, jika tidak maka method akan membuat database baru
      *
-     * @param args 
+     * @param args args
      * @exception IOException menangani error pada file
      */
     public static void main(String[] args) throws IOException{
 
         //membuat database/file yang terhapus
         Database.createNotExistDatabase();
-        
-        // membuka window Root (Window Utama)
+
+        // membuka window MainWindow (Window Utama)
         java.awt.EventQueue.invokeLater(new Runnable(){
 
             @Override
             public void run(){
-                Waktu.updateTime();
-                Aktivitas.addAktivitas("\nAktivitas " + Apps.getUsername() + " pada " + Waktu.getTanggal_Activity());
-                Aktivitas.addAktivitas(Waktu.getTanggal_Activity() +"\t->"+ Apps.getUsername() + " membuka aplikasi.");
-                new com.window.Root().setVisible(true); // membuka window root
+                Aktivitas.addAktivitas("\nAktivitas " + Apps.getUsername() + " pada " + Waktu.getTanggal_Aktivitas());
+                Aktivitas.addAktivitas(Waktu.getTanggal_Aktivitas() +"\t->"+ Apps.getUsername() + " membuka aplikasi.");
+                new MainWindow().setVisible(true); // membuka window root
             }
         });
     }
